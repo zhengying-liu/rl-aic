2e11c7f5453e9b3213f2503d6041de491cc42000
Zhengying Liu <zhengying.liu@polytechnique.edu>
2018-01-05 17:54:02 +0100

    all except thompson

--- tp3/agents/agents.py
+++ tp3/agents/agents.py
@@ -25,35 +25,59 @@ class epsGreedyAgent:
 class optimistEpsGreedyAgent(epsGreedyAgent):
     def __init__(self, A, epsilon, optimism):
         # TO IMPLEMENT
-        raise NotImplementedError
+        self.epsilon = epsilon
+        self.A = A
+        self.Q = np.zeros(A) + 5 # optimistic initial value estimation
+        self.numpick = np.zeros(A)
 
 
 class softmaxAgent:
     def __init__(self, A, temperature):
         # TO IMPLEMENT
-        raise NotImplementedError
+        self.A = A
+        self.temperature = temperature
+        self.Q = np.zeros(A)
+        self.numpick = np.zeros(A)
+        # max of value estimation, to prevent exp(Q) from explosion
+        self.q_max = 0
 
     def interact(self):
         # TO IMPLEMENT
-        raise NotImplementedError
+        powers = (self.Q - self.q_max)/self.temperature
+        energy = np.exp(powers)
+        probability = energy/np.sum(energy)
+        action = np.random.choice(range(self.A), p=probability)
+        return action
 
     def update(self, a, r):
         # TO IMPLEMENT
-        raise NotImplementedError
+        self.numpick[a] += 1
+        self.Q[a] += (r - self.Q[a]) / self.numpick[a]
+        if self.Q[a] > self.q_max:
+            self.q_max = self.Q[a]
 
 
 class UCBAgent:
     def __init__(self, A):
         # TO IMPLEMENT
-        raise NotImplementedError
+        self.timestamp = 1
+        self.A = A
+        self.Q = np.zeros(A)
+        self.numpick = np.zeros(A)
 
     def interact(self):
         # TO IMPLEMENT
-        raise NotImplementedError
+        if np.min(self.numpick) == 0:
+            return np.argmin(self.numpick)
+        else:
+            expectation = self.Q + np.sqrt(2 * np.log(self.timestamp) / self.numpick)
+            return np.argmax(expectation)
 
     def update(self, a, r):
         # TO IMPLEMENT
-        raise NotImplementedError
+        self.timestamp += 1
+        self.numpick[a] += 1
+        self.Q[a] += (r - self.Q[a]) / self.numpick[a]
 
 
 class ThompsonAgent:
